c135f5d84a77674b3194b3d42410540c855f040d
Rakitin-A <74126183+Rakitin-A@users.noreply.github.com>
2022-01-17 14:17:28 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function () {
+
+   
+    $('.product-info__slider').slick({
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false,
+        fade: true,
+        autoplay: true,
+    });
+
+
+});
